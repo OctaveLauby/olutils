74571b29702151cb4383d4d4a08d7f142b8a141c
OctaveLauby <octavelauby@gmail.com>
2020-10-23 14:12:52 +0200

    Remove python 3.6 and 3.7 from lists

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
